6635e316d92bc3f4ff4c69880efec20e622b6f9b
David Anthoff <anthoff@berkeley.edu>
2018-02-10 11:24:25 -0800

    Add initial version

--- src/Electron.jl
+++ src/Electron.jl
@@ -1,5 +1,99 @@
+__precompile__()
 module Electron
 
-# package code goes here
+using JSON
 
-end # module
+export Application, Window
+
+struct Application
+    id::UInt
+    connection
+    proc
+end
+
+struct Window
+    app::Application
+    id::Int
+end
+
+const _global_application = Ref{Nullable{Application}}(Nullable{Application}())
+const _global_application_next_id = Ref{Int}(1)
+
+function __init__()
+    _global_application[] = Nullable{Application}()
+    _global_application_next_id[] = 1
+end
+
+function generate_pipe_name(name)
+    if is_windows()
+        "\\\\.\\pipe\\$name"
+    elseif is_unix()
+        joinpath(tempdir(), name)
+    end
+end
+
+function get_electron_binary_cmd()
+    @static if is_apple()
+        return joinpath(@__DIR__, "..", "deps", "Julia.app", "Contents", "MacOS", "Julia")
+    elseif is_linux()
+        return joinpath(@__DIR__, "..", "deps", "electron", "electron")
+    elseif is_windows()
+        return joinpath(@__DIR__, "..", "deps", "electron", "electron.exe")
+    else
+        error("Unknown platform.")
+    end
+end
+
+function Application()
+    electron_path = get_electron_binary_cmd()
+    mainjs = joinpath(@__DIR__, "main.js")
+    id = _global_application_next_id[]
+    _global_application_next_id[] = id + 1
+    process_id = getpid()
+    pipe_name = "juliaelectron-$process_id-$id"
+    named_pipe_name = generate_pipe_name(pipe_name)
+
+    server = listen(named_pipe_name)
+
+    proc = spawn(`$electron_path $mainjs $pipe_name`)
+
+    sock = accept(server)
+
+    return Application(id, sock, proc)
+end
+
+function Base.close(app::Application)
+    close(app.connection)
+end
+
+function Base.run(app::Application, code::AbstractString)
+    println(app.connection, JSON.json(Dict("target"=>"app", "code"=>code)))
+    retval_json = readline(app.connection)
+    retval = JSON.parse(retval_json)
+    return retval["data"]
+end
+
+function Base.run(win::Window, code::AbstractString)
+    message = Dict("target"=>"window", "winid" => win.id, "code" => code)
+    println(win.app.connection, JSON.json(message))
+    retval_json = readline(win.app.connection)
+    retval = JSON.parse(retval_json)
+    return retval["data"]
+end
+
+function Window(app::Application, url::AbstractString)
+    json_options = JSON.json(Dict("url"=>url))
+    code = "createWindow($json_options)"
+    ret_val = run(app, code)
+    return Window(app, ret_val)
+end
+
+function Window(url::AbstractString)
+    if isnull(_global_application[])
+        _global_application[] = Nullable(Application())
+    end
+
+    return Window(get(_global_application[]), url)
+end
+
+end
---
+++ src/main.js
@@ -0,0 +1,77 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
+const net = require("net")
+
+// Window creation
+var windows = {}
+
+function createWindow(opts) {
+    win = new BrowserWindow(opts)
+    windows[win.id] = win
+    if (opts.url) {
+        win.loadURL(opts.url)
+    }
+    win.setMenu(null)
+
+    // Create a local variable that we'll use in
+    // the closed event handler because the property
+    // .id won't be accessible anymore when the window
+    // has been closed.
+    var win_id = win.id
+
+    win.on('closed', function() {
+        delete windows[win_id]
+    })
+
+    return win.id
+}
+
+function generatePipeName(name) {
+    if (process.platform === 'win32') {
+        return '\\\\.\\pipe\\' + name
+    }
+    else {
+        return path.join(os.tmpdir(), name)
+    }
+}
+
+function process_command(connection, cmd) {
+    if (cmd.target=='app') {
+        retval = eval(cmd.code)
+        connection.write(JSON.stringify({data: retval}) + '\n')
+    }
+    else if (cmd.target=='window') {
+        win = windows[cmd.winid]
+        win.webContents.executeJavaScript(cmd.code).then(function(result) {
+            connection.write(JSON.stringify({data: result}) + '\n')
+        })
+    }
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', function() {
+    buffer = ['']
+
+    connection = net.connect(generatePipeName(process.argv[2]))
+    connection.setEncoding('utf8')
+
+    connection.on('end', function() {
+        app.quit()
+    })
+
+    connection.on('data', function(data) {
+        lines = data.split('\n')
+        buffer[0] += lines[0]
+        for (var i = 1; i < lines.length; i++)
+          buffer[buffer.length] = lines[i]
+
+        while (buffer.length > 1) {
+            cmd_as_json = JSON.parse(buffer.shift())
+            process_command(connection, cmd_as_json)
+        }
+    });
+
+})
