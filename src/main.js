6aac778d800b4a8758a7bb87e7dc62fff3f16a26
David Anthoff <anthoff@berkeley.edu>
2018-02-10 13:04:06 -0800

    Import os in javascript

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
-const net = require("net")
+const net = require('net')
+const os = require('os')
 
 // Window creation
 var windows = {}
