41d6d945d11ade5b32398f0d0ca8fc5b3468c9bb
David Anthoff <anthoff@berkeley.edu>
2018-02-14 17:35:37 -0800

    Increase test coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -23,6 +23,18 @@ res = run(a, "Math.log(Math.exp(1))")
 
 close(w)
 
+w2 = Window(URI("file://test.html"))
+
 close(a)
+
+sleep(1)
+
+w3 = Window(Dict("url" => string(URI("file://test.html"))))
+
+w4 = Window(URI("file://test.html"), options=Dict("title" => "Window title"))
+
+w5 = Window("<body></body>", options=Dict("title" => "Window title"))
+
+close(w3)
 
 end
