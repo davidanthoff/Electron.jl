55566fc8401de57b4e108e9447ad876b9e0a6aa5
David Anthoff <anthoff@berkeley.edu>
2018-02-14 17:19:20 -0800

    Increase test coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,12 +6,23 @@ using Base.Test
 
 w = Window(URI("file://test.html"))
 
+a = applications()[1]
+
 @test isa(w, Window)
+
+@test length(applications()) == 1
+@test length(windows()) == 1
 
 res = run(w, "Math.log(Math.exp(1))")
 
 @test res==1
 
-close(w.app)
+res = run(a, "Math.log(Math.exp(1))")
+
+@test res ==1
+
+close(w)
+
+close(a)
 
 end
