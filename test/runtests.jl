575db96460151313b374bf7a4f1662741bafadef
David Anthoff <anthoff@berkeley.edu>
2018-05-16 12:45:59 -0700

    Don't use @LOCAL in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -40,7 +40,7 @@ close(w)
 @test length(applications()) == 1
 @test isempty(windows(a)) == 1
 
-w2 = Window(@LOCAL("test.html"))
+w2 = Window(URI("file://test.html"))
 
 close(a)
 @test length(applications()) == 1
@@ -50,9 +50,9 @@ sleep(1)
 @test isempty(applications())
 @test isempty(windows(a))
 
-w3 = Window(Dict("url" => string(@LOCAL("test.html"))))
+w3 = Window(Dict("url" => string(URI("file://test.html"))))
 
-w4 = Window(@LOCAL("test.html"), options=Dict("title" => "Window title"))
+w4 = Window(URI("file://test.html"), options=Dict("title" => "Window title"))
 
 w5 = Window("<body></body>", options=Dict("title" => "Window title"))
 
