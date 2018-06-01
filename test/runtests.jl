979e0e7532e74d9ea91d9d03bfc3d9056e60c2f5
David Anthoff <anthoff@berkeley.edu>
2018-06-01 12:40:35 -0700

    Add debug output to tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,7 +19,11 @@ end
 
 @testset "Electron" begin
 
+println("A")
+
 w = Window(URI("file://test.html"))
+
+println("B")
 
 a = applications()[1]
 
@@ -30,29 +34,46 @@ a = applications()[1]
 
 res = run(w, "Math.log(Math.exp(1))")
 
+println("C")
+
 @test res == 1
 
 res = run(a, "Math.log(Math.exp(1))")
 
+println("D")
+
 @test res ==1
 
 close(w)
+
+println("E")
 @test length(applications()) == 1
 @test isempty(windows(a)) == 1
 
+
 w2 = Window(URI("file://test.html"))
+
+println("F")
 
 close(a)
 @test length(applications()) == 1
 @test length(windows(a)) == 0
 
+println("G")
+
 sleep(1)
 @test isempty(applications())
 @test isempty(windows(a))
 
+println("H")
+
 w3 = Window(Dict("url" => string(URI("file://test.html"))))
 
+println("I")
+
 w4 = Window(URI("file://test.html"), options=Dict("title" => "Window title"))
+
+println("J")
 
 w5 = Window("<body></body>", options=Dict("title" => "Window title"))
 
@@ -60,10 +81,17 @@ a2 = applications()[1]
 
 w6 = Window(a2, "<body></body>", options=Dict("title" => "Window title"))
 
+println("K")
+
 close(w3)
+println("L")
 close(w4)
+println("M")
 close(w5)
+println("N")
 close(w6)
+println("O")
 close(a2)
+println("P")
 
 end # testset "Electron"
